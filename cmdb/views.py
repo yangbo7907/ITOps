f87a54a227bac47dd38a3eb9c59223af5c995a94
leoyang <yangbo7907@sina.com>
2017-10-22 15:42:54 +0800

    add form

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from forms import LoginForm
 # Create your views here.
 
 
 def login(request):
-    return render(request,'login.html')
+    form = LoginForm()
+    return render(request,'login.html', {'form':'form'})
