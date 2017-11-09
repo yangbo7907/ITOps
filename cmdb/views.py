8d45b525fc8f6d37c7df4c0371aa36b36a109137
leoyang <yangbo7907@sina.com>
2017-11-09 11:27:50 +0800

    added check if already logged in

--- cmdb/views.py
+++ cmdb/views.py
@@ -5,27 +5,36 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
-
-def user_login(request):        
-    if request.method == 'POST':            
-        form = LoginForm(request.POST)            
-        if form.is_valid():                
-            cd = form.cleaned_data                
-            user = authenticate(username=cd['username'],
-                                password=cd['password'])
-            if user is not None:                    
-                if user.is_active:                        
-                    login(request, user)                        
-#                    return HttpResponse('Authenticated successfully')
-                    return HttpResponseRedirect('/cmdb/index/')
-                else:                        
-                    return HttpResponse('Disabled account')
-        else:            
-            return HttpResponse('Invalid login')        
-    else:            
-        form = LoginForm()        
-        return render(request, 'login.html', {'form': form})
-    
+def user_login(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/cmdb/index/')
+    else:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                user = authenticate(username=cd['username'],
+                                    password=cd['password'])
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                        print(request.session.session_key)
+                        request.session['username'] = cd['username']
+#                       return HttpResponse('Authenticated successfully')
+                        return HttpResponseRedirect('/cmdb/index/')
+                    else:
+                        return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+        else:
+            form = LoginForm()
+            return render(request, 'login.html', {'form': form})
 
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated():
+        print("user.id is: ", request.session['_auth_user_id'])
+        print("username is: " ,request.session['username'])
+        return render(request, 'index.html')
+    else:
+        return HttpResponseRedirect('/cmdb/login')
+
