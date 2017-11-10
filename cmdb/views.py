df6f4981db0f950cab8a5855f355a29d6bbf2089
leoyang <yangbo7907@sina.com>
2017-11-10 09:17:47 +0800

    xxx

---
+++ cmdb/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
+
+# Create your views here.
+
+def user_login(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/cmdb/index/')
+    else:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                print(cd['username'])
+                print(cd['password'])
+                user = authenticate(username=cd['username'],
+                                    password=cd['password'])
+                print('user type: ' + str(type(user)))
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                        print(request.session.session_key)
+                        request.session['username'] = cd['username']
+                        return HttpResponse('Authenticated successfully')
+#                        return HttpResponseRedirect('/cmdb/index/')
+                    else:
+                        return HttpResponse('Disabled account')
+                else:
+                    return HttpResponse('User is None')                   
+            else:
+                return HttpResponse('Invalid login')
+        else:
+            form = LoginForm()
+            return render(request, 'login.html', {'form': form})
+
+def index(request):
+    if request.user.is_authenticated():
+        print("user.id is: ", request.session['_auth_user_id'])
+        print("username is: " ,request.session['username'])
+        return render(request, 'index.html')
+    else:
+        return HttpResponseRedirect('/cmdb/login')
+
