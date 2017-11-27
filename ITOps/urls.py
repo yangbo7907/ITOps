fdaf10c5ab9088439c1bf770cd41b3eda0917910
leoyang <yangbo7907@sina.com>
2017-11-27 19:12:21 +0800

    yyy

--- ITOps/urls.py
+++ ITOps/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 #
 #from cmdb import views
-
+###
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 #    url(r'^login/', views.login),
